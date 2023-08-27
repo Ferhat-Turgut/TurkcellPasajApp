0a5f0db058f5dc20915dfa82547878900cce876b
Ferhat-Turgut <turgutferhat.ft@gmail.com>
2023-08-27 23:53:24 +0300

    added ProductController and EditProduct.cshtml

---
+++ src/WebUI/TurkcellPasajApp.MVC/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TurkcellPasajApp.Services;
+
+namespace TurkcellPasajApp.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IProductService _productService;
+
+        public ProductController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IProductService productService)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> EditProduct(int id)
+        {
+            var product=await _productService.GetProductByIdAsync(id);
+            return View(product);
+        }
+    }
+}
